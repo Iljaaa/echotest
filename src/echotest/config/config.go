987c6290449c5e5c390203541dbcaaf9871ffb1d
ilja <the.ilja@gmail.com>
2022-11-07 13:05:20 +0300

    basic configs

---
+++ src/echotest/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import "fmt"
+
+// Db config
+type dbConfig struct {
+	HOST		string,
+	PORT		string,
+	USER		string,
+	PASSWORD	string
+}
+
+type Configuration struct {
+    DB   dbConfig
+}
+
+var conf Configuration;
+
+func init() {
+	conf = Configuration{}
+	conf.DB = dbConfig{
+			HOST: "asdasdas",
+			PORT: "123",
+			USER: "user",
+			PASSWORD: "password"
+		}
+	// conf = Configuration{
+	// 	db: dbConfig{
+	// 		host: "asdasdas",
+	// 	}
+	// }
+}
+
+func GetConfig() Configuration {
+
+	fmt.Printf("aaa: %+v\n", conf)
+    return conf
+}
--- src/echotest/main.go
+++ src/echotest/main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt";
 import "github.com/Iljaaa/echotest/src/common";
+import "github.com/Iljaaa/echotest/src/config";
 // import "echo_test/common";
 
 func main() {
-    
     fmt.Println("startng test script.....")
     common.Test()
+    // fmt.Sprintf("asdasdas %T", config.Configuration)
+    config := config.GetConfig()
+    a := "DB"
+    fmt.Printf("%+v\n", config.DB)
+    // fmt.Printf("%+v\n", config.db)
+    
 }
